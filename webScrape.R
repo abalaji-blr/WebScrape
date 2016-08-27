3189e712dbba4bc7520c31d970834b000fa53196
abalaji-blr <admin@apples-MacBook-Pro-4.local>
2016-08-27 12:24:38 +0530

    update

--- webScrape.R
+++ webScrape.R
@@ -29,26 +29,66 @@ urlList <- rbindlist(lapply(pageNum, createUrl), fill = TRUE)
 #//*[@id="mainContent"]/div[2]/ol/li[4]/dl/dd[4]
 
 library("httr")
+
+##
+# for the html document, get the relevant x path values.
+getXPathValues <- function(doc, xpathLoc) {
+  locations <- getNodeSet(doc, xpathLoc)
+
+  # get the value
+  result <- sapply(locations, function(x) { xmlValue(x)})  
+}
+
+## Get the URL content
 getUrlContent <- function(url) {
   #doc <- htmlParse(url, isURL = TRUE)
   doc <- htmlParse(rawToChar(GET(url)$content))
   #print(doc)
   #get the locations using the docment's xpath
-  locations <- getNodeSet(doc, '//*[@id="mainContent"]/div/ol/li/dl/dd[3]/span')
-  #print(locations)
-  # get the value
-  data.frame(sapply(locations, function(x) { xmlValue(x)}))
+ 
+  # job location 
+  jobLocations <- getXPathValues(doc, '//*[@id="mainContent"]/div/ol/li/dl/dd[3]/strong')
+  #print(jobLocations)
+ 
+  #job country
+  # if the location is "AnyWhere", the country is empty.
+  # so list is of different size.
+  jobCountryXPath = '//*[@id="mainContent"]/div[2]/ol/li[2]/dl/dd[3]/span'
+  jobCountry <- getXPathValues(doc, jobCountryXPath)
+  #print(jobCountry)
   
+  # title
+  titleDocXPath = '//*[@id="mainContent"]/div/ol/li/dl/dd[2]/strong/a'
+  jobTitle <- getXPathValues(doc, titleDocXPath)
+  #print(jobTitle)
+  
+  # get the date
+  dateDocXPath = '//*[@id="mainContent"]/div/ol/li/dl/dd[4]/strong'
+  jobDate <- getXPathValues(doc, dateDocXPath)
+  #print(jobDate)
+  
+  # job year
+  yearDocXPath = '//*[@id="mainContent"]/div/ol/li/dl/dd[4]/span'
+  jobYear <- getXPathValues(doc, yearDocXPath)
+  #print(jobYear)
+  
+  jobYear <- paste(jobYear, jobDate)
+  data.frame(title = jobTitle, location= jobLocations, date = jobYear)
   #data.frame(content)
 }
 
 #//*[@id="mainContent"]/div[1]/ol/li[6]/dl/dd[3]/span
 
-getUrlContent("http://www.r-users.com/jobs/page/10/" )
+#tt <- getUrlContent("http://www.r-users.com/jobs/page/10/" )
 jobListing <- rbindlist(apply(urlList, 1, getUrlContent))
 
-df <- data.frame(table(jobListing))
+df <- data.frame(table(jobListing$location))
+#df <- data.frame(jobListing)
 
 # print in descending order
 head(df[order(-df$Freq),])
 #//*[@id="mainContent"]/div[2]/ol/li[4]/dl/dd[3]/strong
+
+## get the listing of AnyWhere
+t2 <- jobListing[location == "Anywhere",]
+t3 <- t2[order(t2$date),]
